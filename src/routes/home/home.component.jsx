8c68d5f8db729361b0ce771320b2267e0bbe73b2
Alina Voicu <a.voicu24@gmail.com>
2022-07-30 21:40:44 +0300

    Added basic navigation

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,6 +1,6 @@
 import "../../components/category-item.component";
 import Directory from "../../components/directory/directory.component";
-
+import { Outlet } from "react-router-dom";
 const Home = () => {
   const categories = [
     {
@@ -29,7 +29,12 @@ const Home = () => {
       imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
     },
   ];
-  return <Directory categories={categories} />;
+  return (
+    <div>
+      <Outlet />
+      <Directory categories={categories} />
+    </div>
+  );
 };
 
 export default Home;
