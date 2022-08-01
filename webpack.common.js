2c4b5a3e8c5d527a6628fa3c344457f8c9759597
Alina Voicu <a.voicu24@gmail.com>
2022-08-01 20:50:10 +0300

    Update dependencies

---
+++ webpack.common.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+module.exports = {
+  entry: {
+    index: "./src/scripts/index.ts",
+    library: "./src/scripts/library.ts",
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+    ],
+  },
+};
