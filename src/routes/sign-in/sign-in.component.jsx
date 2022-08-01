741eeb56d153c9409eb94a2a490c54375fc4d132
Alina Voicu <a.voicu24@gmail.com>
2022-08-01 22:13:46 +0300

    Sign in with redirect is working

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
 import {
+  auth,
   signInWithGooglePopup,
   createUserDocumentFromAuth,
+  signInWithGoogleRedirect,
 } from "../../utils/firebase/firebase.utils";
 
 const Signin = () => {
+  useEffect(() => {
+    getRedirectResult(auth).then((response) => {
+      if (response) {
+        createUserDocumentFromAuth(response.user);
+      }
+    });
+  }, []);
+
+  //app unmounts when signInWithGoogleRedirect is called
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
   };
+
   return (
     <div>
       <h1>Sign In Page</h1>
-      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+      <button onClick={signInWithGoogleRedirect}>
+        Sign in with Google Redirect
+      </button>
     </div>
   );
 };
