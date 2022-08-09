b80a1ecb2c8ef76bcdce7d111c105a2f82f6182e
Alina Voicu <a.voicu24@gmail.com>
2022-08-09 19:05:15 +0300

    Added Button component

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -5,7 +5,6 @@ const FormInput = ({ label, ...otherProps }) => {
   return (
     <div className="group">
       <input className="form-input" {...otherProps} />
-
       {label && (
         <label
           className={`${
